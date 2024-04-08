eceb420afea9ec94ac10e56fce11c0fdeed77856
ayush844 <ayush.xyz1625@gmail.com>
2024-04-08 18:40:19 +0530

    setUp for context API
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

---
+++ 11_miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+
+//************************************************************************************************************************ */
+//The Context API in React addresses the issue of prop drilling, where components need to pass data through multiple levels of their component tree. By providing a centralized way to share data across components without the need for explicit prop passing, the Context API streamlines state management and improves code maintainability. It enables developers to create cleaner and more efficient code by eliminating the verbosity and potential errors associated with manual prop passing, especially in deeply nested component structures. Additionally, it facilitates global state management, allowing components to access shared data or state without the need for complex hierarchical communication. Overall, the Context API enhances the scalability and readability of React applications by providing a mechanism for efficient data sharing and state management.  
+//************************************************************************************************************************ */
+
+
+  return (
+    <UserContextProvider>
+      <h1>hello</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
