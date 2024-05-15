6a8a7af94a78b0de91b1959019513c9f6675a8fc
ayush844 <ayush.xyz1625@gmail.com>
2024-05-15 13:06:41 +0530

    revised context API in project 2
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- 12_themeSwitcher/src/contexts/Theme.js
+++ 12_themeSwitcher/src/contexts/Theme.js
@@ -1,15 +1,16 @@
 import { createContext, useContext } from "react";
 
-
+// setting default values
 export const ThemeContext = createContext({
     themeMode: 'light',
     darkTheme: ()=>{},
     lightTheme: ()=>{}
 });
 
+// exporting provider wrapper from our ThemeContext
 export const ThemeProvider = ThemeContext.Provider;
 
-
+// making a custom hook to acces the context
 export default function useTheme(){
     return useContext(ThemeContext)
 }
